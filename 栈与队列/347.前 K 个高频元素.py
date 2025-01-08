1f1a3c01d3897fe45d02161d736fa860b7559539
yukinorin775780 <97926996+yukinorin775780@users.noreply.github.com>
2025-01-08 21:15:58 +0800

    Update 347.前 K 个高频元素.py

--- 栈与队列/347.前 K 个高频元素.py
+++ 栈与队列/347.前 K 个高频元素.py
@@ -1,3 +1,4 @@
+# 使用小顶堆
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         # 统计元素出现的频率
@@ -18,3 +19,27 @@ class Solution:
         for i in range(k-1, -1, -1):
             result[i] = heapq.heappop(pri_que)[1]
         return result
+
+# 使用字典
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # 使用字典统计数字出现的次数
+        freq_dict = defaultdict(int)
+        for num in nums:
+            freq_dict[num] += 1
+        # 更改字典，key为出现次数，value为对应的数字
+        index_dict = defaultdict(list)
+        for key in freq_dict:
+            index_dict[freq_dict[key]].append(key)
+        # 排序
+        key = list(index_dict.keys())
+        key.sort()
+        result = []
+        cnt = 0
+        # 获取前k项
+        while key and cnt != k:
+            result += index_dict[key[-1]]
+            cnt += len(index_dict[key[-1]])
+            key.pop()
+        
+        return result[0:k]
