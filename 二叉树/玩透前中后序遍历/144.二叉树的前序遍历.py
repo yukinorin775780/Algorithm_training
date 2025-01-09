cdc47eae5fc66fe262259732264277510a9216c2
yukinorin775780 <97926996+yukinorin775780@users.noreply.github.com>
2025-01-09 18:54:17 +0800

    Update 144.二叉树的前序遍历.py

--- 二叉树/玩透前中后序遍历/144.二叉树的前序遍历.py
+++ 二叉树/玩透前中后序遍历/144.二叉树的前序遍历.py
@@ -1,3 +1,4 @@
+# 递归法
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         res = []
@@ -12,3 +13,23 @@ class Solution:
         
         dfs(root)
         return res
+
+# 迭代法
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # 根节点为空返回空列表
+        if not root:
+            return []
+        stack = [root]
+        result = []
+        while stack:
+            node = stack.pop()
+            # 中节点先处理
+            result.append(node.val)
+            # 右子树先入栈
+            if node.right:
+                stack.append(node.right)
+            # 左子树再入栈
+            if node.left:
+                stack.append(node.left)
+        return result
