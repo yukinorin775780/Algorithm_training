9bf751652bb065b9cc9d9a16fcf88c9081ac327e
yukinorin775780 <97926996+yukinorin775780@users.noreply.github.com>
2025-01-09 19:47:33 +0800

    Update 94.二叉树的中序遍历.py

--- 二叉树/玩透前中后序遍历/94.二叉树的中序遍历.py
+++ 二叉树/玩透前中后序遍历/94.二叉树的中序遍历.py
@@ -1,3 +1,4 @@
+# 递归法
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         res = []
@@ -12,3 +13,25 @@ class Solution:
         
         dfs(root)
         return res
+
+# 迭代法
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+
+        if not root:
+            return []
+        stack = [] # 不能提前将root节点加入stack中
+        result = []
+        cur = root
+        while cur or stack:
+            # 先迭代访问最底层的左子树节点
+            if cur:
+                stack.append(cur)
+                cur = cur.left
+            # 到达最左节点后处理栈顶节点
+            else:
+                cur = stack.pop()
+                result.append(cur.val)
+                # 取栈顶元素右节点
+                cur = cur.right
+        return result
