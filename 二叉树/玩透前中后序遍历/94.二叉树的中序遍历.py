be9fb83c41a6cd0736789a964314e04d691fd0d5
yukinorin775780 <97926996+yukinorin775780@users.noreply.github.com>
2025-01-09 18:48:07 +0800

    Create 94.二叉树的中序遍历.py

---
+++ 二叉树/玩透前中后序遍历/94.二叉树的中序遍历.py
@@ -0,0 +1,14 @@
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+
+        def dfs(node):
+            if node is None:
+                return
+            
+            dfs(node.left)
+            res.append(node.val)
+            dfs(node.right)
+        
+        dfs(root)
+        return res
