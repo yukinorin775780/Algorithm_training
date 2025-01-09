926155bbafd40f2eb1e9a0f96faa8d4d69d7b46d
yukinorin775780 <97926996+yukinorin775780@users.noreply.github.com>
2025-01-09 18:59:04 +0800

    Update 145.二叉树的后序遍历.py

--- 二叉树/玩透前中后序遍历/145.二叉树的后序遍历.py
+++ 二叉树/玩透前中后序遍历/145.二叉树的后序遍历.py
@@ -1,3 +1,4 @@
+# 递归法
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         res = []
@@ -12,3 +13,26 @@ class Solution:
         
         dfs(root)
         return res
+
+
+# 迭代法
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # 根节点为空则返回空列表
+        if not root:
+            return []
+        
+        stack = [root]
+        result = []
+        while stack:
+            node = stack.pop()
+            # 先处理中节点
+            result.append(node.val)
+            # 左子树先进栈
+            if node.left:
+                stack.append(node.left)
+            # 右子树再进栈
+            if node.right:
+                stack.append(node.right)
+        # 最终将数组反转   前序遍历是 中左右 -(调整代码左右顺序)-> 中右左 -(反转)-> 左右中
+        return result[::-1]
