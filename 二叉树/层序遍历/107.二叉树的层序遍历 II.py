68089db614c101558f4e9bc858e6d434ceedbeb9
yukinorin775780 <97926996+yukinorin775780@users.noreply.github.com>
2025-01-09 23:05:43 +0800

    Create 107.二叉树的层序遍历 II.py

---
+++ 二叉树/层序遍历/107.二叉树的层序遍历 II.py
@@ -0,0 +1,17 @@
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        queue = collections.deque([root])
+        result = []
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                cur = queue.popleft()
+                level.append(cur.val)
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+            result.append(level)
+        return result[::-1]
