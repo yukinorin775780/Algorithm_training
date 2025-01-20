2dc48f3904eaf7e0a3a875c712ef2cf61ea349cf
yukinorin775780 <97926996+yukinorin775780@users.noreply.github.com>
2025-01-20 18:54:00 +0800

    Create 236.二叉树的最近公共祖先.py

---
+++ 二叉树/236.二叉树的最近公共祖先.py
@@ -0,0 +1,34 @@
+# 后序遍历
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == p or root == q or not root:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+        if not left and right:
+            return right
+        elif left and not right:
+            return left
+        else:
+            return None
+
+ # 后序精简版
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == p or root == q or not root:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+
+        if not left:
+            return right
+        
+        return left
