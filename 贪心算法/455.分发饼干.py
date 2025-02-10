5b77c4018020ff04e1971f41a323d32a1422fd8b
yukinorin775780 <97926996+yukinorin775780@users.noreply.github.com>
2025-02-10 14:58:12 +0800

    Update 455.分发饼干.py

--- 贪心算法/455.分发饼干.py
+++ 贪心算法/455.分发饼干.py
@@ -4,9 +4,9 @@ class Solution:
         g.sort()    # 将小孩按胃口大小排序
         s.sort()    # 将饼干按尺寸大小排序
         index = 0
-        result = 0
-        for i in range(len(g)): # 遍历小孩
-            if index < len(s) and s[index] >= g[i]: # 饼干尺寸大于小孩胃口
-                result += 1
+        # result = 0
+        for i in range(len(s)): # 遍历饼干
+            if index < len(g) and g[index] <= s[i]: # 小孩胃口小于饼干尺寸
+                # result += 1
                 index += 1
         return result
