8a0a554f55f775d6cadbe8b64b15b081c283c509
yukinorin775780 <97926996+yukinorin775780@users.noreply.github.com>
2025-02-18 23:05:54 +0800

    Create 746.使用最小花费爬楼梯.py

---
+++ 动态规划/746.使用最小花费爬楼梯.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        """
+        1. 确定dp[i]定义：到达下标i的位置所需要的花费为dp[i]
+        2. 确定递推公式：dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
+        3. 初始化dp[i]: dp[0] = 0, dp[1] = 0
+        4. 确定遍历顺序：从前到后
+        5. 打印dp数组
+        """
+        dp = [0] * (len(cost) + 1)
+        dp[0], dp[1] = 0, 0
+
+        for i in range(2, len(cost)+1):
+            dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
+        
+        return dp[len(cost)]
