2857797b1ad9ab5b5d364ca70655fb9fc517d397
yukinorin775780 <97926996+yukinorin775780@users.noreply.github.com>
2025-04-16 10:39:13 +0800

    Update 647.回文子串.py

--- 动态规划/647.回文子串.py
+++ 动态规划/647.回文子串.py
@@ -1,3 +1,4 @@
+# 方法1: 动态规划
 class Solution:
     def countSubstrings(self, s: str) -> int:
         """
@@ -21,3 +22,24 @@ class Solution:
                         dp[i][j] = True
         
         return result
+
+
+
+# 方法2: 双指针
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        result = 0
+        for i in range(len(s)):
+            result += self.extend(s, i, i, len(s)) # 以i为中心
+            result += self.extend(s, i, i+1, len(s)) # 以i和i+1为中心
+        
+        return result
+
+    def extend(self, s, i ,j ,n):
+        res = 0
+        while i >= 0 and j < n and s[i] == s[j]:
+            i -= 1
+            j += 1
+            res += 1
+        return res
+
